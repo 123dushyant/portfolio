2ca8875bfb1ced20e6cd1918186c348b00294597
apple <apple@apples-MacBook-Pro.local>
2024-08-05 00:28:50 +0530

    fix: some bug

--- src/components/AboutContent.jsx
+++ src/components/AboutContent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const AboutContent = () => {
     return (
         <div className=" relative flex-1 lg:pl-40 pr-12  pl-6 pt-10 lg:pt-44 lg:max-w-4xl lg:text-left mx-auto text-lg lg:text-2xl max-w-lg sm:pt-20  tracking-widest font-thin ">
-            <h2 className='' >I am proficient in technologies such as HTML, CSS, <br /> JavaScript, React, and Node.js, and I am constantly <br /> expanding my skill set to stay abreast of the latest <br /> industry trends. Collaboration and effective <br /> communication are at the core of my work ethic, allowing me to understand client needs and translate them into technical specifications and actionable plans. <br /> <br />
+            <h2 className='' >I am proficient in technologies such as HTML, CSS,  JavaScript, React, and Node.js, and I am constantly expanding my skill set to stay abreast of the latest <br /> industry trends. Collaboration and effective <br /> communication are at the core of my work ethic, allowing me to understand client needs and translate them into technical specifications and actionable plans. <br /> <br />
             Beyond my technical prowess, I am a strong advocate for learning and developing good stuff, ensuring that my work not only meets but exceeds expectations. I am dedicated to continuous learning and professional development, staying abreast of industry trends and advancements to remain at the forefront of my field.</h2>
         </div>
     );

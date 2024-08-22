2a0ee666b4d4870c5bcc2b79f4c84f9f98cd0e40
apple <apple@apples-MacBook-Pro.local>
2024-08-22 10:00:10 +0530

    fix: some bug

--- src/Pages/Work.jsx
+++ src/Pages/Work.jsx
@@ -9,10 +9,10 @@ const Work = () => {
             <div className="header font-extrabold text-slate-200 text-8xl  lg:text-9xl xl:text-[22vw] w-full h-screen bg-customGray top-0 sticky text-center flex items-center justify-center">
                 <h1 id="scale1" className="scale-y">WORK</h1>
             </div>
-            <div className="flex flex-wrap justify-center gap-4 p-0 bg-customGray  lg:p-0">
+            <div className="flex flex-wrap justify-center gap-4 p-4 lg:p-0">
                 <div className="card flex flex-col items-center bg-gray-800 rounded-lg overflow-hidden shadow-lg">
                     <img src={work1} alt="card-img1" className="card-img w-full h-auto object-cover" />
-                    <div className="card-content text-white p-4 flex items-center justify-center  text-base lg:text-lg">
+                    <div className="card-content text-white p-4 flex items-center justify-center text-base lg:text-lg">
                         <div className="visit border-2 w-28 items-center flex justify-center border-solid border-white rounded-full">
                             <a href="https://123dushyant.github.io/Ecommerce-website/" target="_blank" rel="noopener noreferrer" className=" w-full h-full flex items-center justify-center">
                                 <button className="w-full h-full ">Visit-</button>
@@ -36,7 +36,7 @@ const Work = () => {
                 <h1 className=" text-5xl font-serif mt-24 mb-[-70px] ml-5">LET'S START</h1>
                 <h1 className="text-5xl sm:text-6xl ml-40 font-serif mt-[-60px] text-center mr-[470px]">CREATE</h1>
                 <span className="text-5xl font-serif mt-[-130px] ml-5">TOGETHER</span>
-                <span className=" text-[36px] font-extrabold ml-4 sm:ml-40 sm:text-9xl">LET'S COLLABORATE </span>
+                <span className=" text-[34px] font-extrabold ml-4 sm:ml-40 sm:text-9xl">LET'S COLLABORATE </span>
             </div>
             
         </div>
